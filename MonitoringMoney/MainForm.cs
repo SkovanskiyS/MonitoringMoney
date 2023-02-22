6d81d7af9d700626c1e117806ebdc85a6380744d
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-02-22 21:49:33 +0500

    git init

--- MonitoringMoney/MainForm.cs
+++ MonitoringMoney/MainForm.cs
@@ -10,12 +10,19 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Bunifu.UI.WinForms;
+using MySql.Data.MySqlClient;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace MonitoringMoney
 {
     public partial class MainForm : Form
     {
+        DataBase dB;
+        DataTable dataTable;
+        MySqlDataAdapter dataAdapter;
+        MySqlCommand cmd;
+
+        string dateOfAdd,clientName, get_give, currency, amountOfMoney, well, cash_transferText;
         public MainForm()
         {
             InitializeComponent();
@@ -24,6 +31,18 @@ namespace MonitoringMoney
         private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            dateOfAdd = dateOfReg.Text;
+            clientName = clientNameT.Text;
+            get_give = this.get_giveDropdown.Text;
+            currency = currency_Dropdown.Text;
+            amountOfMoney = sumValue.Text;
+            well = wellText.Text;
+            cash_transferText = cash_transfer.Text;
+
         }
 
         private void bunifuTextBox1_KeyPress(object sender, KeyPressEventArgs e)
@@ -61,7 +80,7 @@ namespace MonitoringMoney
 
             //    MessageBox.Show(e.Message,"Ошибка",MessageBoxButtons.AbortRetryIgnore,MessageBoxIcon.Error)
             //}
-            string selectedItem = Convert.ToString(currency_Dropdown.SelectedItem);
+            string selectedItem = currency_Dropdown.Text;
             string clear_value = sumValue.Text.Trim();
             try
             {
@@ -89,7 +108,18 @@ namespace MonitoringMoney
 
         private void bunifuButton21_Click(object sender, EventArgs e)
         {
-            
+            //dB = new DataBase();
+            //dataTable = new DataTable();
+            //dataAdapter = new MySqlDataAdapter();
+            //cmd = new MySqlCommand("SELECT * FROM `log_pass` WHERE `login` = @username AND `password` = @password", dB.getConnection());
+
+            //string user_name = this.user_box.Text;
+            //string user_password = this.password_box.Text;
+
+            //cmd.Parameters.Add("@username", MySqlDbType.VarChar).Value = user_name;
+            //cmd.Parameters.Add("@password", MySqlDbType.VarChar).Value = user_password;
+            MessageBox.Show(cash_transfer.Text);
+            //dataAdapter.SelectCommand = cmd;
         }
     }
 }
